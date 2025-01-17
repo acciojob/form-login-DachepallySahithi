29c9575a3b77bf02dc41241e7f71946449a4a96b
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-17 17:11:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function getFormvalue() {
     //Write your code here
-	const firstNameInput = document.getElementById('firstName');
-	const lastNameInput = document.getElementById('lastName');
+	event.preventDefault();
+	const firstName = document.getElementById('firstName').value;
+	const lastName = document.getElementById('lastName').value;
 	
-	const firstName = firstNameInput.value;
-    const lastName = lastNameInput.value;
+	// const firstName = firstNameInput.value;
+ //    const lastName = lastNameInput.value;
 
-    const fullName = firstName + " " + lastName;
-	alert(fullName);
+ //    const fullName = firstName + " " + lastName;
+	alert(firstName +' ' + lastName);
 }
