f8c315d7bf41895b94cbc6152730f48d0f794d2b
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-17 14:36:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getFormvalue() {
     //Write your code here
-	var firstName = document.getElementById("firstName").value;
-	var lastName = document.getEementById("lastName").value;
+	var firstName = document.getElementById("fname").value;
+	var lastName = document.getEementById("lname").value;
 	alert(firstName + " " + lastName);
 }
